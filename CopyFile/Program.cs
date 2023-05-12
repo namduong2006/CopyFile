dc74ddd2042275bcccf05b6af50984d6422679c4
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-05-12 19:40:36 +0700

    Initial commit

---
+++ CopyFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter source file: ");
+            string sourcePath = Console.ReadLine();
+            Console.WriteLine("Enter destination file: ");
+            string destinationPath = Console.ReadLine();
+
+            FileInfo source = new FileInfo(sourcePath);
+            FileInfo des = new FileInfo(destinationPath);
+            try
+            {
+                CopyFile(source, des);
+                Console.WriteLine("Copy Completed");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Cannot Copy");
+                Console.Error.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+        private static void CopyFile(FileInfo source, FileInfo dest)
+        {
+            Console.WriteLine("Start Copy using FileInfo");
+            source.CopyTo(dest.FullName, true);
+        }       
+    }   
+}
